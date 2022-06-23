958b98fe5e945f8f16dee8a1cba26399a2822f30
AndyNotfound <Andy.Notfound@gmail.com>
2022-06-24 05:14:06 +0700

    Done!

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const time = document.querySelector('#time');
 const score = document.querySelector('#score');
 
 let result = 0;
+let currentTime = 60;
+let timerId = null;
+let hitPosition;
 
 function randomSquare(){
     squares.forEach(square => {
@@ -11,11 +14,35 @@ function randomSquare(){
     });
     let randomSquare = squares[Math.floor(Math.random() * 9)]
     randomSquare.classList.add('mole');
+
+    hitPosition = randomSquare.id;
 };
 
+squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+        if (square.id === hitPosition){
+            result++
+            score.innerText = result;
+            hitPosition = null;
+        }
+    })
+})
+
 function moveMole (){
-    let timerId = null;
     timerId = setInterval(randomSquare, 500);
 }
 
 moveMole ();
+
+function countDown (){
+    currentTime--
+    time.textContent = currentTime;
+
+    if (currentTime == 0){
+        clearInterval(countDownTimerId);
+        clearInterval(timerId);
+        alert('Game Over!');
+    }
+}
+
+let countDownTimerId = setInterval(countDown, 1000);
